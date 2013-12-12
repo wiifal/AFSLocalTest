f2ac8adab5533a0d5acf74364312244b9b8f1f57
francisco <franciscoal2@hotmail.com>
2013-12-12 14:47:48 +0100

    Second Additions

--- FirstApp/mainForm.Designer.cs
+++ FirstApp/mainForm.Designer.cs
@@ -31,20 +31,21 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.txtText = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lstLista = new System.Windows.Forms.ListBox();
+            this.lblOne = new System.Windows.Forms.Label();
+            this.lstList = new System.Windows.Forms.ListBox();
+            this.chkColorChange = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(35, 35);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(581, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(343, 22);
             this.dateTimePicker1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(506, 204);
+            this.button1.Location = new System.Drawing.Point(268, 204);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 49);
             this.button1.TabIndex = 1;
@@ -56,45 +57,64 @@
             // 
             this.txtText.Location = new System.Drawing.Point(126, 63);
             this.txtText.Name = "txtText";
-            this.txtText.Size = new System.Drawing.Size(490, 22);
+            this.txtText.Size = new System.Drawing.Size(252, 22);
             this.txtText.TabIndex = 2;
             // 
-            // label1
+            // lblOne
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "New Label:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblOne.AutoSize = true;
+            this.lblOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOne.Location = new System.Drawing.Point(32, 66);
+            this.lblOne.Name = "lblOne";
+            this.lblOne.Size = new System.Drawing.Size(88, 17);
+            this.lblOne.TabIndex = 3;
+            this.lblOne.Text = "New Label:";
+            this.lblOne.Click += new System.EventHandler(this.label1_Click);
             // 
-            // lstLista
+            // lstList
             // 
-            this.lstLista.FormattingEnabled = true;
-            this.lstLista.ItemHeight = 16;
-            this.lstLista.Items.AddRange(new object[] {
+            this.lstList.FormattingEnabled = true;
+            this.lstList.ItemHeight = 16;
+            this.lstList.Items.AddRange(new object[] {
             "One",
             "Two",
-            "Three"});
-            this.lstLista.Location = new System.Drawing.Point(35, 105);
-            this.lstLista.Name = "lstLista";
-            this.lstLista.Size = new System.Drawing.Size(280, 148);
-            this.lstLista.TabIndex = 4;
-            this.lstLista.UseTabStops = false;
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten"});
+            this.lstList.Location = new System.Drawing.Point(35, 105);
+            this.lstList.Name = "lstList";
+            this.lstList.Size = new System.Drawing.Size(137, 148);
+            this.lstList.TabIndex = 4;
+            this.lstList.UseTabStops = false;
             // 
-            // Form1
+            // chkColorChange
+            // 
+            this.chkColorChange.AutoSize = true;
+            this.chkColorChange.Location = new System.Drawing.Point(262, 105);
+            this.chkColorChange.Name = "chkColorChange";
+            this.chkColorChange.Size = new System.Drawing.Size(116, 21);
+            this.chkColorChange.TabIndex = 5;
+            this.chkColorChange.Text = "Change Color";
+            this.chkColorChange.UseVisualStyleBackColor = true;
+            this.chkColorChange.CheckedChanged += new System.EventHandler(this.chkColorChange_CheckedChanged);
+            // 
+            // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 291);
-            this.Controls.Add(this.lstLista);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(401, 291);
+            this.Controls.Add(this.chkColorChange);
+            this.Controls.Add(this.lstList);
+            this.Controls.Add(this.lblOne);
             this.Controls.Add(this.txtText);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
-            this.Name = "Form1";
+            this.Name = "mainForm";
             this.Text = "GitSample";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -106,8 +126,9 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtText;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox lstLista;
+        private System.Windows.Forms.Label lblOne;
+        private System.Windows.Forms.ListBox lstList;
+        private System.Windows.Forms.CheckBox chkColorChange;
     }
 }
 

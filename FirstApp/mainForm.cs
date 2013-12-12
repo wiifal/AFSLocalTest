551fcd530d7c53d1fd29d8d651075e32d4960c9d
francisco <franciscoal2@hotmail.com>
2013-12-12 14:42:28 +0100

    SecondChangeWave

--- FirstApp/mainForm.cs
+++ FirstApp/mainForm.cs
@@ -19,12 +19,28 @@ namespace FirstApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            txtText.Text = "Hello world sample. This is a source control." + lstLista.Items[lstLista.SelectedIndex].ToString();
+            if (lstList.SelectedIndex != -1)
+            {
+                txtText.Text = "This is a source control." + lstList.Items[lstList.SelectedIndex].ToString();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
+            lblOne.Text = "Clicked!:";
+        }
 
+        private void chkColorChange_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkColorChange.Checked == true)
+            {
+                txtText.ForeColor = Color.Red;
+                label1_Click(sender, e);
+            }
+            else
+            {
+                txtText.ForeColor = Color.Black;
+            }
         }
     }
 }

64b2d8612082deec74d170b8f09fe5a75f6bc352
francisco <franciscoal2@hotmail.com>
2013-12-12 14:23:11 +0100

    NewAdditions

--- FirstApp/mainForm.Designer.cs
+++ FirstApp/mainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FirstApp
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -31,42 +31,71 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.txtText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstLista = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Location = new System.Drawing.Point(35, 35);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(322, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(581, 22);
             this.dateTimePicker1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(226, 192);
+            this.button1.Location = new System.Drawing.Point(506, 204);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(199, 23);
+            this.button1.Size = new System.Drawing.Size(110, 49);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Pulsame";
+            this.button1.Text = "Press me!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtText
             // 
-            this.txtText.Location = new System.Drawing.Point(35, 73);
+            this.txtText.Location = new System.Drawing.Point(126, 63);
             this.txtText.Name = "txtText";
-            this.txtText.Size = new System.Drawing.Size(322, 22);
+            this.txtText.Size = new System.Drawing.Size(490, 22);
             this.txtText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "New Label:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstLista
+            // 
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.ItemHeight = 16;
+            this.lstLista.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three"});
+            this.lstLista.Location = new System.Drawing.Point(35, 105);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(280, 148);
+            this.lstLista.TabIndex = 4;
+            this.lstLista.UseTabStops = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(639, 291);
+            this.Controls.Add(this.lstLista);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtText);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GitSample";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +106,8 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstLista;
     }
 }
 
